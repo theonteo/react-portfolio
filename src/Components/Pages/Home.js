e1c0e5cc866ef843777ba2749cb3f93e1d8811d6
theonteo <56335249+theonteo@users.noreply.github.com>
2021-06-19 23:15:33 +0800

    info 2

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -3,11 +3,12 @@ import '../../App.css';
 import HeroSection from '../HeroSection';
 import Cards from '../Cards';
 import Footer from '../Footer';
+import InformationSection from '../InformationSection';
 function Home()
 {
     return (
         <>
-            
+            <InformationSection/>
         </>
     );
 }
