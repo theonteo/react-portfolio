03d9339ab23ef0811c87c190d86c386184de4a73
theonteo <56335249+theonteo@users.noreply.github.com>
2021-06-26 04:18:34 +0800

    footer update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,10 +9,10 @@ function Footer() {
     <div className='footer-container'>
       <section className='footer-subscription'>
         <p className='footer-subscription-heading'>
-          Join the Adventure newsletter to receive our best vacation deals
+          Enter your email for the latest updates
         </p>
         <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
+          Contact me for for more information
         </p>
         <div className='input-areas'>
           <form>
@@ -28,48 +28,33 @@ function Footer() {
       </section>
       <div class='footer-links'>
         <div className='footer-link-wrapper'>
+         
           <div class='footer-link-items'>
-            <h2>About Us</h2>
-            <Link to='/sign-up'>How it works</Link>
-            <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
-            <Link to='/'>Investors</Link>
-            <Link to='/'>Terms of Service</Link>
-          </div>
-          <div class='footer-link-items'>
-            <h2>Contact Us</h2>
-            <Link to='/'>Contact</Link>
-            <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
-            <Link to='/'>Sponsorships</Link>
+            <h2>Contact Me</h2>
+            <Link to='/'>LinkedIn</Link>
+            <Link to='/'>GitHub</Link>
           </div>
         </div>
         <div className='footer-link-wrapper'>
           <div class='footer-link-items'>
-            <h2>Videos</h2>
-            <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
+            <h2>Navigate</h2>
+            <Link to='/'>Home</Link>
+            <Link to='/'>Projects</Link>
+            <Link to='/'>Contact</Link>
+            <Link to='/'>Resume</Link>
           </div>
-          <div class='footer-link-items'>
-            <h2>Social Media</h2>
-            <Link to='/'>Instagram</Link>
-            <Link to='/'>Facebook</Link>
-            <Link to='/'>Youtube</Link>
-            <Link to='/'>Twitter</Link>
-          </div>
+          
         </div>
       </div>
       <section class='social-media'>
         <div class='social-media-wrap'>
           <div class='footer-logo'>
             <Link to='/' className='social-logo'>
-              TRVL
+              THEON
               <i class='fab fa-typo3' />
             </Link>
           </div>
-          <small class='website-rights'>TRVL © 2020</small>
+          <small class='website-rights'>Theon © 2021</small>
           <div class='social-icons'>
             <Link
               class='social-icon-link facebook'
