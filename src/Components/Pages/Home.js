dd4b83f04153a9182ac10f5f25e90b9d6d36f4f2
theonteo <56335249+theonteo@users.noreply.github.com>
2021-06-27 23:13:36 +0800

    website push

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../../App.css';
 
-
+import Footer from '../Footer';
 import Landing from '../Landing';
 function Home()
 {
     return (
         <>
           <Landing/>
-         
+          <Footer/>
         </>
     );
 }
