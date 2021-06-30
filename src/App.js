8153ffb2272537ac6face6987f24866fba7a15f1
theonteo <56335249+theonteo@users.noreply.github.com>
2021-06-30 21:54:38 +0800

    add test page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from "./Components/Pages/Home"
 import Services from "./Components/Pages/Services"
 import Products from "./Components/Pages/Products"
 import SignUp from "./Components/Pages/SignUp"
+import Test from "./Components/Pages/Test"
 
 //renders html
 function App() {
@@ -20,6 +21,7 @@ function App() {
         <Route path ='/services'exact  component = {Services}/>
         <Route path ='/products'exact  component = {Products}/>
         <Route path ='/sign-up'exact  component = {SignUp}/>
+        <Route path ='/test'exact  component = {Test}/>
       </Switch>
   </Router>
   );
--- src/Components/LandingTitle.js
+++ src/Components/LandingTitle.js
@@ -5,15 +5,18 @@ function LandingTitle()
  {
     return (
         <div>
+            <div className = 'Title-colored'>Hi, my name is</div>
             <div className = 'Title-heading'>
-                TITLE 
+                Theon Teo
             </div>
-            <div className = 'Title-heading'>
+            <div className = 'Title-SubHeading'>
                
-                 PAGE
+                 Software Engineer
             </div>
             <div className = 'Title-subtext'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            Starting out from game development, I am currently pursuing software engineering / development with a focus in front-end aspects. My interests are in Front-End Graphics Rendering / Technical Art and Full-stack development.
+
+During my free-time from schoolwork, I enjoy exploring the different aspects of frontend web/app development. 
 
             </div>
         </div>
