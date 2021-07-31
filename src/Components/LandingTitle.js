4286ffb00edc541325b3bca405b91c97b0a3ec33
theonteo <56335249+theonteo@users.noreply.github.com>
2021-07-31 22:28:39 +0800

    text test

--- src/Components/LandingTitle.js
+++ src/Components/LandingTitle.js
@@ -8,21 +8,15 @@ function LandingTitle()
     return (
         
         <div className = 'Title-group'>
-           <li className="Title-subtext">hello <span>There</span>.</li>
-<li className="Title-subtext">hi!<span>welcome back</span> <i>MR0B</i> to intro.</li>
+
             <div className = 'Title-colored'>Hi, my name is</div>
            
             <div className = 'Title-heading'>
                 Theon Teo
             </div>
-            <div className = 'Title-SubHeading'>
+            <div className = 'Title-heading2'>
                
                  Software Engineer
-            </div>
-            <div className = 'Title-subtext'>
-            Starting out from game development, I am currently pursuing software engineering / development with a focus in front-end aspects. My interests are in Front-End Graphics Rendering / Technical Art and Full-stack development.
-
-During my free-time from schoolwork, I enjoy exploring the different aspects of frontend web/app development. 
             </div>
             <div className = 'Title-subtext'>
             Starting out from game development, I am currently pursuing software engineering / development with a focus in front-end aspects. My interests are in Front-End Graphics Rendering / Technical Art and Full-stack development.
