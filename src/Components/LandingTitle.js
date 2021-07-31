f5e568f2b0b7a7748fdb5bc1e9ba761d85cddf8b
theonteo <56335249+theonteo@users.noreply.github.com>
2021-07-31 19:57:51 +0800

    landin tweak

--- src/Components/LandingTitle.js
+++ src/Components/LandingTitle.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import {React} from 'react'
+
 import './LandingTitle.css'
 import './font.css'
 function LandingTitle()
  {
+     
     return (
-        <div>
+        
+        <div className = 'Title-group'>
+           <li className="Title-subtext">hello <span>There</span>.</li>
+<li className="Title-subtext">hi!<span>welcome back</span> <i>MR0B</i> to intro.</li>
             <div className = 'Title-colored'>Hi, my name is</div>
+           
             <div className = 'Title-heading'>
                 Theon Teo
             </div>
@@ -17,9 +23,14 @@ function LandingTitle()
             Starting out from game development, I am currently pursuing software engineering / development with a focus in front-end aspects. My interests are in Front-End Graphics Rendering / Technical Art and Full-stack development.
 
 During my free-time from schoolwork, I enjoy exploring the different aspects of frontend web/app development. 
+            </div>
+            <div className = 'Title-subtext'>
+            Starting out from game development, I am currently pursuing software engineering / development with a focus in front-end aspects. My interests are in Front-End Graphics Rendering / Technical Art and Full-stack development.
 
+During my free-time from schoolwork, I enjoy exploring the different aspects of frontend web/app development. 
             </div>
         </div>
+        
     )
 }
 
