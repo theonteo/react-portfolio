18bb05548ca5f9389928a551f3db3b37d8d3a698
theonteo <56335249+theonteo@users.noreply.github.com>
2021-08-25 16:13:47 +0800

    wip

--- src/Components/LandingTitle.js
+++ src/Components/LandingTitle.js
@@ -5,21 +5,19 @@ import {Button} from '../Components/NavBar/Button'
 function LandingTitle()
  {
     return (
-        /*
+        
         <div>
         <div className = 'Title-heading'>
-            TITLE 
+        ../TheonTeo/
         </div>
-        <div className = 'Title-heading'>
-             PAGE
-        </div>
+
         <div className = 'Title-subtext'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        <Button buttonStyle='btn--outline'> Contact</Button>
 
         </div>
     </div>
-    */
-
+    
+        /*
         <div className = 'Title-group'>
             <div className = 'Title-heading'>
                 ../TheonTeo/
@@ -31,6 +29,7 @@ function LandingTitle()
           
             <Button buttonStyle='btn--outline'> Contact</Button>
         </div>
+        */
     )
 }
 
