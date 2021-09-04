bf45b58badffe2336f132e3ed34d52f8aff00605
theonteo <56335249+theonteo@users.noreply.github.com>
2021-09-05 02:22:12 +0800

    remove landing title

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import LandingTitle from './LandingTitle'
 import Scene1 from './Scenes/Scene1'
 import Scene2 from './Scenes/Scene2'
-//import Scene3 from './Scenes/Scene3'
 import Projects from './Projects'
 import About from './About'
 import './Landing.css'
