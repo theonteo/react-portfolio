c9dde2eb8f777aabd1d5e086c36e4eabfa0f1a6a
theonteo <56335249+theonteo@users.noreply.github.com>
2021-11-01 02:51:56 +0800

    roomba madness

--- src/Components/Pages/Project-Portfolio.js
+++ src/Components/Pages/Project-Portfolio.js
@@ -17,25 +17,45 @@ import { Button } from '../Interaction/Button'
 
 export default function ProjectPortfolio()
 {
-    const goToTop = ()=>{
+    const goToTop = () =>
+    {
         document.documentElement.scrollTop = 0;
-      };
+    };
     return (
         <>
             <div className='page-background'>
                 <div className='page-container'>
+                    <div className='page-bold'>
+                        Work In Progress...
+                    </div>
+                    <div className='page-image-tag'>
+                        ../Blog post
+                    </div>
                     <div className='page-title'>
                         Building a portfolio site
                     </div>
                     <div className='page-label'>
                         A blog on the creation of the website you're viewing now
                     </div>
-                    <div className='page-bold'>
-                        Work In Progress.
+                    <div className='page-bold-nospace'>
+                        Initial Prototype.
                     </div>
-                    
+                    <div className='page-text'>
+                        During the ideations phase,several ideas are being explored as to what format the portfolio would be in.
+                        One of the idea would be a full blown game where you play as a roomba "cleaning up a codebase" while exploring the various projects.
+                        The game will be built with playcanvas,a web based game engine.This idea is quickly scrapped in favour of a proper webpage to learn frontend frameworks.
+
+                    </div>
+                    <div className='page-text'>
+                        Thankfully <b>the prototype still exist</b> and you can play it below!
+                    </div>
+
+                    <iframe src="https://playcanv.as/p/bfb6df11/" title='play'></iframe>
+                    <div className='page-label'>
+                        Controls - WASD to move. | Only playable in desktop.
+                    </div>
                     <div className='page-text'>Want to see more?</div>
-                    <Button buttonStyle='btn--outline' path='/'  onClick={goToTop} > Return to Home Page</Button>
+                    <Button buttonStyle='btn--outline' path='/' onClick={goToTop} > Return to Home Page</Button>
                     <div className='page-text'></div>
                 </div>
             </div>
--- src/Components/Pages/Project-Simple.js
+++ src/Components/Pages/Project-Simple.js
@@ -19,15 +19,16 @@ import ImgBeauty from '../../Images/img-beauty.jpg'
 
 export default function ProjectSimple()
 {
-    const goToTop = ()=>{
+    const goToTop = () =>
+    {
         document.documentElement.scrollTop = 0;
-      };
+    };
     return (
         <>
             <div className='page-background'>
                 <div className='page-container'>
-                <div className='page-image-tag'>
-                       ../Blog post
+                    <div className='page-image-tag'>
+                        ../Blog post
                     </div>
                     <div className='page-title'>
                         SIMPLE. Engine
@@ -43,7 +44,7 @@ export default function ProjectSimple()
                         Development for the engine began in 2019 and has since been through multiple iterations before the current version you will see below.
                     </div>
                     <div class="project-img">
-                        <img src={ImgSimple} alt="" style = {{ filter:'grayscale(0%)'}}/>
+                        <img src={ImgSimple} alt="" style={{ filter: 'grayscale(0%)' }} />
                     </div>
                     <div className='page-image-tag'>
                         Editor interface of Simple Engine 2020.
@@ -51,7 +52,7 @@ export default function ProjectSimple()
                     <div className='page-bold'>
                         Simple Engine Technical Showcase 2021
                     </div>
-                    <iframe title = 'rendertest' width src="https://www.youtube.com/embed/jCnMN9iv688"></iframe>
+                    <iframe title='rendertest' width src="https://www.youtube.com/embed/jCnMN9iv688"></iframe>
                     <div className='page-image-tag'>
                         First 3D Render Test for Simple Engine 2021.
                     </div>
@@ -73,10 +74,10 @@ export default function ProjectSimple()
                         Own developed features for the engine
                     </div>
                     <div class="project-img">
-                        <img src={ImgBeauty} alt=""style = {{ filter:'grayscale(0%)'}} />
+                        <img src={ImgBeauty} alt="" style={{ filter: 'grayscale(0%)' }} />
                     </div>
                     <div className='page-image-tag'>
-                       Test render in engine.
+                        Test render in engine.
                     </div>
                     <div className='page-text'>
                         Through the project duration,there are multiple attempt to build unique features that will enhance fidelity of the environment.
@@ -94,25 +95,25 @@ export default function ProjectSimple()
 
                     <div className='page-text'>
                         To speed up environment creation, mesh painting brush features were added to complement existing graphical features.
-                        </div> 
-                        <div className='page-text'>
+                    </div>
+                    <div className='page-text'>
                         Such painting features would include:
                         <li>Vertex Position Painting</li>
                         <li>Vertex Color Painting</li>
                         <li>Mesh geometry painting</li>
                         Mesh Painting demo video below.
-                    </div> 
-                    <iframe title = 'rendershading' width src="https://www.youtube.com/embed/oV1LBBaEWfA"></iframe>
+                    </div>
+                    <iframe title='rendershading' width src="https://www.youtube.com/embed/oV1LBBaEWfA"></iframe>
                     <div className='page-bold'>
                         Shading and Lighting
                     </div>
-                    <iframe title = 'rendertechnical' width src="https://www.youtube.com/embed/0BT9SjFj0OU"></iframe>
+                    <iframe title='rendertechnical' width src="https://www.youtube.com/embed/0BT9SjFj0OU"></iframe>
                     <div className='page-bold'>
                         Technical Art
                     </div>
-                    <iframe title = 'render' width src="https://www.youtube.com/embed/jCnMN9iv688"></iframe>
+                    <iframe title='render' width src="https://www.youtube.com/embed/jCnMN9iv688"></iframe>
                     <div className='page-text'>Want to see more?</div>
-                    <Button buttonStyle='btn--outline' path='/'  onClick={goToTop} > Return to Home Page</Button>
+                    <Button buttonStyle='btn--outline' path='/' onClick={goToTop} > Return to Home Page</Button>
                     <div className='page-text'></div>
                 </div>
             </div>
